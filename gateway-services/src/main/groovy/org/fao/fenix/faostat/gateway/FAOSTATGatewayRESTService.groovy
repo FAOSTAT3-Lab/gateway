4716a2396b4832e50a2c6c2febda0338b7bce436
Vortex- <simone.murzilli@fao.org>
2014-04-25 16:57:50 +0200

    gateway update

--- gateway-services/src/main/groovy/org/fao/fenix/faostat/gateway/FAOSTATGatewayRESTService.groovy
+++ gateway-services/src/main/groovy/org/fao/fenix/faostat/gateway/FAOSTATGatewayRESTService.groovy
@@ -37,55 +37,144 @@ import org.fao.fenix.faostat.gateway.*;
 @Path("")
 class FAOSTATGatewayRESTService {
 
-
+    // DEFAULT config file and language
     def CONFIG_FILE = 'config/faostat/config.json'
+    def DEFAULT_LANGUAGE = 'EN'
 
-    def DEFAULT_LANGUAGE = 'E'
+    // Variables to change on HTML files
+    def $_GATEWAY_REPO_BASE_URL = '$_GATEWAY_REPO_BASE_URL'
+    def $_MODULES_BASE_URL = '$_MODULES_BASE_URL'
+    def $_CONTENT = '$_CONTENT' // placeholder in the HTML to change the HTML module
+    def $_ISO2_LANG = '$_ISO2_LANG'
+    def $_FAOSTAT_LANG = '$_FAOSTAT_LANG'
+    def $_CODE = '$_CODE' // code for group/domain etc
+    def $_MODULE_CODE = '$_MODULE_CODE'
+    def $MODULE_CONFIG = '$MODULE_CONFIG'
+
+    // search
+    def $_WORD = '$_WORD'
+    def $_DISPLAY_SEARCH = '$_DISPLAY_SEARCH'
+
 
     /**
-     * @param section   FAOSTAT section: home
-     * @param lang      E, F or S
+     * @param module    FAOSTAT module: home
+     * @param lang      EN, FR or ES
      * @return          HTML code to be rendered by the browser
      */
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("home")
-    String loadModuleHomeDefault() { return loadModuleHome(DEFAULT_LANGUAGE) }
+    public String loadModuleHomeDefault() { return loadModuleHome(DEFAULT_LANGUAGE) }
 
     /**
     /**
-     * @param section   FAOSTAT section: home
+     * @param module   FAOSTAT module: home
      * @param lang      E, F or S
      * @return          HTML code to be rendered by the browser
      */
     @GET
     @Produces(MediaType.TEXT_HTML)
-    @Path("home/{lang}")
-    String loadModuleHomeLang(@PathParam("lang") String lang) { return loadModuleHome(lang) }
-    String loadModuleHome(lang) {
-        // Fetch the base HTML
-        HashMap<String, String> configMap = readConfigFile();
-        String main = replaceHtml(configMap, 'faostat-home-js', lang);
-        main = main.replace('$_GATEWAY_REPO_BASE_URL', configMap.get("GATEWAY_REPO_BASE_URL"))
-        // Return the page
-        return main
+    @Path("{lang}/home")
+    public String loadModuleHomeLang(@PathParam("lang") String lang) { return loadModuleHome(lang) }
+
+    private String loadModuleHome(lang) {
+        return replaceHtml(readConfigFile(), 'faostat-home-js', 'home', lang);
     }
+
+    /*************************************************************
+     *
+     * BROWSE
+     *
+     ***************************************************************/
+
+
+    //TODO: a default view also for /browse?
+    /*@GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("browse")
+    public String loadBrowseDefault() {
+        loadBrowse(DEFAULT_LANGUAGE, 'domain', null)
+    }
+
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{lang}/browse")
+    public String loadLangBrowseDefault(@PathParam("lang") String lang) {
+        loadBrowse(lang, 'domain', null)
+    } */
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{lang}/browse/{section}")
+    public String loadLangBrowseSection(@PathParam("lang") String lang, @PathParam("section") String section) {
+        loadBrowse(lang, section, null)
+    }
+
+    // TODO: needed?
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("browse/{section}")
+    public String loadBrowseSection(@PathParam("section") String section) {
+        loadBrowse(DEFAULT_LANGUAGE, section, null)
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/browse/{section}/{code}")
+    public String loadBrowseSectionCode(@PathParam("section") String section, @PathParam("code") String code) {
+        loadBrowse(DEFAULT_LANGUAGE, section, code)
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("{lang}/browse/{section}/{code}")
+    public String loadBrowseSectionCodeLang(@PathParam("lang") String lang, @PathParam("section") String section, @PathParam("code") String code) {
+        loadBrowse(lang, section, code)
+    }
+
+   private String loadBrowse(String lang, String section, String code) {
+       println('------------')
+       println(lang)
+       println(section)
+       println(code)
+
+       HashMap<String, String> configMap = readConfigFile();
+       String main = replaceHtml(configMap, 'faostat-browse-js', 'browse', lang);
+
+       // config module (TODO: make it in json)
+       def config = '{ "SECTION" : "' + section + '", "CODE" : "' + code + '"}'
+       main = main.replace($MODULE_CONFIG, config )
+       //println(main)
+
+       // Return the page
+       return main
+   }
+
+
+
+
+
+
+
+
 
 
 
     /**
-     * @param section   FAOSTAT section: browse, download, search, compare, analysis, mes, home
+     * @param module    FAOSTAT module: browse, download, search, compare, analysis, mes, home
      * @param group     FAOSTAT DB group
      * @param domain    FAOSTAT DB group
      * @param lang      E, F or S
      * @return          HTML code to be rendered by the browser
      */
-    @GET
+    /**@GET
     @Produces(MediaType.TEXT_HTML)
-    @Path("/{section}/{group}/{domain}/{lang}")
-    String loadModule(@PathParam("section") String section, @PathParam("group") String group, @PathParam("domain") String domain, @PathParam("lang") String lang) {
+    @Path("/{module}/{group}/{domain}/{lang}")
+    String loadModule(@PathParam("module") String module, @PathParam("group") String group, @PathParam("domain") String domain, @PathParam("lang") String lang) {
+        println('HERE')
         HashMap<String, String> configMap = readConfigFile();
-        String main = replaceHtml(configMap, 'faostat-'+ section +'-js', lang);
+        String main = replaceHtml(configMap, 'faostat-'+ module +'-js', module, lang);
         main = main.replace('$_GROUP_CODE', (group == "*" ? "null" : group));
         main = main.replace('$_DOMAIN_CODE', (domain == "*" ? "null" : domain));
         def faostatLang = getFAOSTATLang(lang)
@@ -93,11 +182,11 @@ class FAOSTATGatewayRESTService {
         main = main.replace('$_ISO2_LANG', lang)
         // Return the page
         return main
-    }
+    }  **/
 
 
     /**
-     * @param section   FAOSTAT section: browse, download, search, compare, analysis, mes
+     * @param module   FAOSTAT module: browse, download, search, compare, analysis, mes
      * @param word      The word to search ( i.e. 'rice')
      * @param lang      E, F or S
      * @return          HTML code to be rendered by the browser
@@ -107,8 +196,8 @@ class FAOSTATGatewayRESTService {
     @Path("search/{word}/{lang}")
     String loadModuleSearch(@PathParam("word") String word, @PathParam("lang") String lang) {
         HashMap<String, String> configMap = readConfigFile();
-        String main = replaceHtml(configMap, 'faostat-search-js', lang);
-        main = main.replace('$_WORD', (word == "*" ? "" : word));
+        String main = replaceHtml(configMap, 'faostat-search-js', 'search', lang);
+        main = main.replace($_WORD, (word == "*" ? "" : word));
         // Return the page
         return main
     }
@@ -135,36 +224,38 @@ class FAOSTATGatewayRESTService {
         return "en";
     }
 
-    String replaceHtml(configMap, section, lang) {
+    String replaceHtml(configMap, modulepath, modulecode, lang) {
 
         // set lang always as toUpperCase
         lang = lang.toUpperCase();
 
         // HTLM CONTENT
-        def main = new URL('http://' + configMap.get("GATEWAY_REPO_BASE_URL") + "/faostat/base_index.html").getText()
+        def main = new URL('http://' + configMap.get($_GATEWAY_REPO_BASE_URL) + "/faostat/base_index.html").getText()
 
-        main = main.replace('$_GATEWAY_REPO_BASE_URL', configMap.get("GATEWAY_REPO_BASE_URL"))
-        main = main.replace('$_MODULES_BASE_URL', configMap.get("MODULES_BASE_URL"))
+        main = main.replace($_MODULE_CODE, modulecode)
+        main = main.replace($_GATEWAY_REPO_BASE_URL, configMap.get($_GATEWAY_REPO_BASE_URL))
+        main = main.replace($_MODULES_BASE_URL, configMap.get($_MODULES_BASE_URL))
 
         def faostatLang = getFAOSTATLang(lang)
-        main = main.replace('$_LANG', faostatLang)
-        main = main.replace('$_ISO2_LANG', lang)
+        main = main.replace($_FAOSTAT_LANG, faostatLang)
+        main = main.replace($_ISO2_LANG, lang)
 
         // TODO: make it nicer
-        main = main.replace('$_DISPLAY_SEARCH', 'true')
+        main = main.replace($_DISPLAY_SEARCH, 'true')
 
         // Set page's title
-        main = main.replace('$_SECTION_NAME', "home")
+        // main = main.replace('$_SECTION_NAME', "home")
 
         // Load the module
-        def sectionContent = new URL('http://' + configMap.get("GATEWAY_REPO_BASE_URL") + "/faostat/" + section +"/index_gateway.html").getText();
+        def moduleContent = new URL('http://' + configMap.get($_GATEWAY_REPO_BASE_URL) + "/faostat/" + modulepath +"/index_gateway.html").getText();
 
         // Replace wildcards with parameters from the REST
-        sectionContent = sectionContent.replace('$_ISO2_LANG', faostatLang)
-        sectionContent = sectionContent.replace('GATEWAY_REPO_BASE_URL', configMap.get("GATEWAY_REPO_BASE_URL"))
+        moduleContent = moduleContent.replace($_ISO2_LANG, faostatLang)
+        moduleContent = moduleContent.replace($_GATEWAY_REPO_BASE_URL, configMap.get($_GATEWAY_REPO_BASE_URL))
+
 
         // Inject the module into the main HTML
-        main = main.replace('$_CONTENT', sectionContent);
+        main = main.replace($_CONTENT, moduleContent);
         return main;
     }
 
